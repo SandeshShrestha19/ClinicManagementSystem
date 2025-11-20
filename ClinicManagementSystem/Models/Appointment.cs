11c4c14e801ebb81f7d3721400b4b0efd6879dd9
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-20 14:37:35 +0545

    feat: add properties for appointment entity

---
+++ ClinicManagementSystem/Models/Appointment.cs
@@ -0,0 +1,19 @@
+﻿
+using ClinicManagementSystem.Models.Enums;
+
+namespace ClinicManagementSystem.Models
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; }
+
+        public Patient Patient { get; set; }
+        public Guid PatientId { get; set; }
+        public Doctor Doctor { get; set; }
+        public Guid DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Description { get; set; }
+        public AppointmentStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
