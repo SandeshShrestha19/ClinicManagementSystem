0e072dc0fa9f0eea3a20e383ce8b3e1655c992a6
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-20 14:34:33 +0545

    feat: add service for database connectionString

---
+++ ClinicManagementSystem/Extensions/DbContextExtension.cs
@@ -0,0 +1,14 @@
+﻿using ClinicManagementSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManagementSystem.Extensions
+{
+    public static class DbContextExtension
+    {
+        public static IServiceCollection AddDatabaseDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("Default")));
+            return services;
+        }
+    }
+}
