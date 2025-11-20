47350cbd5bbbb93cba70e2e96bdcbe26d10412eb
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-20 14:33:21 +0545

    feat: add DbSet entities for creating tables in database

---
+++ ClinicManagementSystem/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ClinicManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManagementSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        // You can use this method as well
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Appointment>(b =>
+            {
+                b.ToTable("Appointments");
+            });
+        }
+        */
+    }
+}
